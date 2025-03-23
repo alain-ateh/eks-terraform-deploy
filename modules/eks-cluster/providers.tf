7f4271f275407489ac924c04ce0717cd4a79d306
alain-ateh <mrmeloateh@yahoo.com>
2025-03-22 23:48:04 -0400

    deploy using development branch

--- modules/eks-cluster/providers.tf
+++ modules/eks-cluster/providers.tf
@@ -3,11 +3,11 @@ provider "aws" {
   region  = "us-east-2"
 }
 
-data "aws_eks_cluster" "this" {
+data "aws_eks_cluster" {
   name = var.cluster_name
 }
 
-data "aws_eks_cluster_auth" "this" {
+data "aws_eks_cluster_auth" {
   name = var.cluster_name
 }
 
